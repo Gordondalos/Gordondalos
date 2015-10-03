5bc5019508ad3558f6c3436ac8b9d5b5f5023b7f
gordondalos <bondarenko.kg@mail.ru>
2015-10-03 19:23:24 +0600

    что то доделал

---
+++ js/myScrypt.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+
+    // Кликаю по ссылкам до перехода на блоки
+    $("a#I").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $('.'+elementClick).offset().top-100;
+        if ($.browser.safari) {
+            $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость
+        } else {
+            $('html').animate({ scrollTop: destination }, 1100);
+        }
+        return false;
+    });
+// что я делаю
+    $("a#do").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $('.'+elementClick).offset().top-100;
+        if ($.browser.safari) {
+            $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость
+        } else {
+            $('html').animate({ scrollTop: destination }, 1100);
+        }
+        return false;
+    });
+// уменя навыки
+    $("a#skill").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $('.'+elementClick).offset().top-100;
+        if ($.browser.safari) {
+            $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость
+        } else {
+            $('html').animate({ scrollTop: destination }, 1100);
+        }
+        return false;
+    });
+// опыт
+    $("a#experience").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $('.'+elementClick).offset().top-100;
+        if ($.browser.safari) {
+            $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость
+        } else {
+            $('html').animate({ scrollTop: destination }, 1100);
+        }
+        return false;
+    });
+// работы
+    $("a#work").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $('.'+elementClick).offset().top-100;
+        if ($.browser.safari) {
+            $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость
+        } else {
+            $('html').animate({ scrollTop: destination }, 1100);
+        }
+        return false;
+    });
+// контакты
+    $("a#contact").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $('.'+elementClick).offset().top-100;
+        if ($.browser.safari) {
+            $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость
+        } else {
+            $('html').animate({ scrollTop: destination }, 1100);
+        }
+        return false;
+    });
+
+
+
+    //new WOW().init();
+
+   // $('.lamp').addClass('wow bounceInLeft');
+   // $('.myfoto').addClass('wow bounceInRight');
+   // $('.skill').addClass('wow zoomIn');
+   //
+   //// $('.lamp').addClass('wow bounceInRight');
+   // $('.green').addClass('wow rollIn');
+   // $('.work').addClass('wow RotateInDownRight');
+   // $('.contact').addClass('wow bounceInRight');
+
+
+});
