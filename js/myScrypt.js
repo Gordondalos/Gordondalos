f4416902650ad015696b603a7eea7b9278050b3c
gordondalos <bondarenko.kg@mail.ru>
2015-10-21 10:24:30 +0600

    слил

--- js/myScrypt.js
+++ js/myScrypt.js
@@ -1,66 +1,72 @@
 $(document).ready(function () {
+    var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+    var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
+    var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+    var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+    var is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
 
-    // Кликаю по ссылкам до перехода на блоки
     $("a#I").click(function () {
         var elementClick = $(this).attr("href");
         var destination = $('.'+elementClick).offset().top-100;
-        if ($.browser.safari) {
-            $('body').animate({ scrollTop: destination }, 5000); //1100 - скорость
+        if (is_safari) {
+            $('body').animate({ scrollTop: destination }, 5000); //1100 - Г±ГЄГ®Г°Г®Г±ГІГј
         } else {
             $('html').animate({ scrollTop: destination }, 5000);
         }
         return false;
     });
-// что я делаю
-    $("a#do").click(function () {
+
+    $("a#skills").click(function () {
         var elementClick = $(this).attr("href");
         var destination = $('.'+elementClick).offset().top-100;
-        if ($.browser.safari) {
-            $('body').animate({ scrollTop: destination }, 5000); //1100 - скорость
+
+
+        if (is_safari) {
+            $('body').animate({ scrollTop: destination }, 5000); //1100 - Г±ГЄГ®Г°Г®Г±ГІГј
         } else {
             $('html').animate({ scrollTop: destination }, 5000);
         }
         return false;
     });
-// уменя навыки
-    $("a#skill").click(function () {
+
+    $("a#portfolio").click(function () {
         var elementClick = $(this).attr("href");
         var destination = $('.'+elementClick).offset().top-100;
-        if ($.browser.safari) {
-            $('body').animate({ scrollTop: destination }, 5000); //1100 - скорость
+        if (is_safari) {
+            $('body').animate({ scrollTop: destination }, 5000); //1100 - Г±ГЄГ®Г°Г®Г±ГІГј
         } else {
             $('html').animate({ scrollTop: destination }, 5000);
         }
         return false;
     });
-// опыт
+
     $("a#experience").click(function () {
         var elementClick = $(this).attr("href");
         var destination = $('.'+elementClick).offset().top-100;
-        if ($.browser.safari) {
-            $('body').animate({ scrollTop: destination }, 5000); //1100 - скорость
+        if (is_safari) {
+            $('body').animate({ scrollTop: destination }, 5000); //1100 - Г±ГЄГ®Г°Г®Г±ГІГј
         } else {
             $('html').animate({ scrollTop: destination }, 5000);
         }
         return false;
     });
-// работы
-    $("a#work").click(function () {
+
+    $("a#tech").click(function () {
         var elementClick = $(this).attr("href");
-        var destination = $('.'+elementClick).offset().top-100;
-        if ($.browser.safari) {
-            $('body').animate({ scrollTop: destination }, 5000); //1100 - скорость
+        var destination = $('.'+elementClick).offset().top-1200;
+        if (is_safari) {
+            $('body').animate({ scrollTop: destination }, 5000); //1100 - Г±ГЄГ®Г°Г®Г±ГІГј
         } else {
             $('html').animate({ scrollTop: destination }, 5100);
         }
         return false;
     });
-// контакты
+
     $("a#contact").click(function () {
         var elementClick = $(this).attr("href");
         var destination = $('.'+elementClick).offset().top-100;
-        if ($.browser.safari) {
-            $('body').animate({ scrollTop: destination }, 10000); //1100 - скорость
+        if (is_safari) {
+            $('body').animate({ scrollTop: destination }, 10000); //1100 - Г±ГЄГ®Г°Г®Г±ГІГј
         } else {
             $('html').animate({ scrollTop: destination }, 10000);
         }
@@ -68,17 +74,19 @@ $(document).ready(function () {
     });
 
 
+    $('.section_9 img').addClass('wow zoomIn');
+    $('.section_6 img').addClass('wow zoomIn');
 
     //new WOW().init();
 
-   // $('.lamp').addClass('wow bounceInLeft');
-   // $('.myfoto').addClass('wow bounceInRight');
-   // $('.skill').addClass('wow zoomIn');
-   //
-   //// $('.lamp').addClass('wow bounceInRight');
-   // $('.green').addClass('wow rollIn');
-   // $('.work').addClass('wow RotateInDownRight');
-   // $('.contact').addClass('wow bounceInRight');
+    // $('.lamp').addClass('wow bounceInLeft');
+    // $('.myfoto').addClass('wow bounceInRight');
+    // $('.skill').addClass('wow zoomIn');
+    //
+    //// $('.lamp').addClass('wow bounceInRight');
+    // $('.green').addClass('wow rollIn');
+    // $('.work').addClass('wow RotateInDownRight');
+    // $('.contact').addClass('wow bounceInRight');
 
 
 });
---
+++ js/paralax.js
@@ -0,0 +1,60 @@
+(function($) {
+
+    $.fn.myParallax = function( options ) {
+
+        var settings = $.extend({
+            "speed" : "15"
+        }, options);
+
+        return this.each(function() {
+
+            var ths = $(this);
+
+            ths
+                .css({
+
+                    "position" : "relative",
+                    "overflow" : "hidden"
+                })
+                .wrapInner("<div class='parallax-content' style='position:relative;z-index:1'>")
+                .prepend("<div class='image-parallax' style='background-image:url(" + ths.data('parallax-image') + ");background-size:cover;background-position:top;position:absolute;top:0;width:100%'>");
+
+            function parallaxInit() {
+
+                var pheight = ths.height();
+
+                ths.children(".image-parallax").css({
+                    "height" : pheight*2,
+                    "top" : -pheight
+                });
+
+                var st = $(document).scrollTop();
+                var sp = ths.offset().top - $(window).height();
+                var ob = ths.offset().top + pheight;
+                var sr = st-sp;
+
+                if(st >= sp && st <= ob) {
+
+                    ths.children(".image-parallax").css({
+                        "transform" : "translate3d(0px, " + sr /settings.speed + "%, .01px)",
+                        "-webkit-transform" : "translate3d(0px, " + sr /settings.speed + "%, .01px)"
+                    });
+
+                };
+
+            };
+
+            $(window).scroll(function() {
+                parallaxInit();
+            }).load(function() {
+                parallaxInit();
+            });
+            $("*").resize(function() {
+                parallaxInit();
+            });
+
+        });
+
+    };
+
+})($);
