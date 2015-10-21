adb126554386587db6c0b7c60e970eeaf6b5d1f2
gordondalos <bondarenko.kg@mail.ru>
2015-10-21 13:04:30 +0600

    сжал файлы ксс

--- config.rb
+++ config.rb
@@ -7,6 +7,8 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "imgs"
 javascripts_dir = "js"
+output_style = :compressed
+
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
